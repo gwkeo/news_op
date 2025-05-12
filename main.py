f90f8209eb7acda4a5d640a12222973d1c046bdd
Rakhman Imanov <raxman.i@mail.ru>
2025-05-12 20:40:39 +0300

    Реализован парсинг всех уникальных статейных ссылок из новостей

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from parsers import dzen_parser
 from drivers import chrome_driver
-from transformers import pipeline
+# from transformers import pipeline
 
-model = pipeline(
-    model="r1char9/rubert-base-cased-russian-sentiment",
-)
+# model = pipeline(
+#     model="r1char9/rubert-base-cased-russian-sentiment",
+# )
+
 
 if __name__ == "__main__":
     driver = chrome_driver.init_driver()
     news = dzen_parser.get_news(driver=driver, url="https://dzen.ru/topic/moy-krasnodar")
+    links = set()
     for i in news:
-        print(i, model(i))
-        print()
-    print(len(news))
+        links = dzen_parser.get_news_url_links(driver, i, links)
+    print(links)
+    print(len(links))
+
+
--- parsers/dzen_parser.py
+++ parsers/dzen_parser.py
@@ -1,19 +1,43 @@
 from bs4 import BeautifulSoup
 
+
 def get_news(driver, url):
-    driver.get(url)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features='html.parser')
-    links = soup.select('[class^="desktop2--widget-news-desktop__mainNews"]')
-    links.extend(soup.select('[class^="desktop2--card-top-avatar__rootElement"]'))
-    result = []
-    for i in links:
-        try:
-            result.append(i.find('p').text)
-        except:
-            result.append(i.select('[class^="desktop2--card-news__titleLink"]')[0].get_attribute_list('title')[0])
+    try:
+        driver.get(url)
+        content = driver.page_source
+        soup = BeautifulSoup(content, features='html.parser')
+        links = soup.select('[class^="desktop2--widget-news-desktop__mainNews"]')
+        links.extend(soup.select('[class^="desktop2--card-top-avatar__rootElement"]'))
+        links.extend(soup.select('[class^="desktop2--card-horizontal-news__cardLink"]'))
+        result = []
+        for i in links:
+            if i.get_attribute_list('href'):
+                result.append(i.get_attribute_list('href')[0])
+            else:
+                try:
+                    result.append(i.select('[class^="desktop2--card-news__titleLink"]')[0].get_attribute_list('href')[0])
+                except:
+                    continue
+    except:
+        print(f"something went wrong: '{i}'")
     return result
 
-def get_news_links(driver, url):
-    driver.get(url)
-    content = driver.page_source
+
+def get_news_url_links(driver, url, set_of_urls):
+    try:
+        driver.get(url)
+        content = driver.page_source
+        soup = BeautifulSoup(content, features='html.parser')
+        articles = soup.select('[class^="news-site--StorySummarization-desktop__item"]')
+        links = []
+        for i in articles:
+            links = i.select('a')
+            for link in links:
+                a = link.get_attribute_list('href')[0]
+                if 'dzen.ru' in a:
+                    set_of_urls.add(a)
+        return set_of_urls
+    except:
+        raise Exception("something went wrong")
+
+
